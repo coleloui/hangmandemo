94aca8c283ac64aa35632f8e3156864ce6fb073e
coleloui <coleloui18@gmail.com>
2021-09-24 10:10:48 -0700

    starting files

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+var wordBlank = document.querySelector(".word-blanks");
+var win = document.querySelector(".win");
+var lose = document.querySelector(".lose");
+var timerElement = document.querySelector(".timer-count");
+var startButton = document.querySelector(".start-button");
+
+var chosenWord = "";
+var numBlanks = 0;
+var winCounter = 0;
+var loseCounter = 0;
+var isWin = false;
+var timer;
+var timerCount;
+
+var lettersInChosenWord = [];
+var blanksLetters = [];
+
+var words = ["variable","array", "modulus", "object", "function", "string", "boolean"];
+
+
